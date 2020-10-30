3904a3ef75c0b5fc70f84505051caed81e7871ab
Ram Krishnan <ramkrishnan@live.com>
2020-10-30 19:45:25 +0530

    Initialise auth routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 const app = express();
@@ -8,5 +9,7 @@ const app = express();
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () =>
     console.log("Connected to the Database")
 );
+
+app.use("/user", authRoute);
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
