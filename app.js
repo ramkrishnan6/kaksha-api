61c88ad9f313cf89117da6f0181da9d949790201
Ram Krishnan <ramkrishnan@live.com>
2020-11-03 12:51:57 +0530

    Update socket code to support start and end class

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const dashboardRoute = require("./routes/dashboard");
+const classRoute = require("./routes/class");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
+const Class = require("./models/Class");
 
 dotenv.config();
 const app = express();
@@ -19,6 +21,7 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () =>
 app.use(cors());
 app.use(express.json());
 app.use("/user", authRoute, dashboardRoute);
+app.use("/class", classRoute);
 
 io.use(async (socket, next) => {
     try {
@@ -34,33 +37,64 @@ io.use(async (socket, next) => {
 
 const rooms = {};
 
-function addClientToMap(userName, socketId, roomId, tp) {
+function addClientToMap(userName, socketId, roomId, role) {
     if (!rooms[roomId]) {
-        rooms[roomId] = new Map();
+        rooms[roomId] = {};
+        rooms[roomId]["students"] = new Map();
+        rooms[roomId]["teachers"] = new Map();
     }
 
-    if (!rooms[roomId].has(userName)) {
-        rooms[roomId].set(userName, new Set([socketId]));
-    } else {
-        rooms[roomId].get(userName).add(socketId);
+    if (role == "student") {
+        if (!rooms[roomId]["students"].has(userName)) {
+            rooms[roomId]["students"].set(userName, new Set([socketId]));
+        } else {
+            rooms[roomId]["students"].get(userName).add(socketId);
+        }
+    }
+
+    if (role == "teacher") {
+        if (!rooms[roomId]["teachers"].has(userName)) {
+            rooms[roomId]["teachers"].set(userName, new Set([socketId]));
+        } else {
+            rooms[roomId]["teachers"].get(userName).add(socketId);
+        }
     }
     console.log(rooms);
 }
 
-function removeClientFromMap(userName, socketId, roomId) {
-    if (rooms[roomId].has(userName)) {
-        let userSocketIdSet = rooms[roomId].get(userName);
+function removeClientFromMap(userName, socketId, roomId, role) {
+    if (role == "student") {
+        if (rooms[roomId]["students"].has(userName)) {
+            let userSocketIdSet = rooms[roomId]["students"].get(userName);
 
-        userSocketIdSet.delete(socketId);
+            userSocketIdSet.delete(socketId);
 
-        if (userSocketIdSet.size == 0) {
-            rooms[roomId].delete(userName);
+            if (userSocketIdSet.size == 0) {
+                rooms[roomId]["students"].delete(userName);
+            }
+            console.log(rooms);
+
+            // if (rooms[roomId].size == 0) {
+            //     delete rooms[roomId];
+            // }
         }
-        console.log(rooms);
+    }
 
-        // if (rooms[roomId].size == 0) {
-        //     delete rooms[roomId];
-        // }
+    if (role == "teacher") {
+        if (rooms[roomId]["teachers"].has(userName)) {
+            let userSocketIdSet = rooms[roomId]["teachers"].get(userName);
+
+            userSocketIdSet.delete(socketId);
+
+            if (userSocketIdSet.size == 0) {
+                rooms[roomId]["teachers"].delete(userName);
+            }
+            console.log(rooms);
+
+            // if (rooms[roomId].size == 0) {
+            //     delete rooms[roomId];
+            // }
+        }
     }
 }
 server.listen(8000, async () => {
@@ -76,20 +110,47 @@ io.on("connection", (socket) => {
     socket.on("join-room", (roomId) => {
         let user = users.find((u) => u._id == socket.userId);
 
-        let i = 1;
-        if (i == 0) {
-            socket.emit("end");
-            socket.disconnect();
-        }
-
-        addClientToMap(user["first_name"], socket.id, roomId);
+        addClientToMap(user["first_name"], socket.id, roomId, user["role"]);
         socket.join(roomId);
-        io.sockets.in(roomId).emit("user-connected", [...rooms[roomId].keys()]);
-        socket.on("disconnect", () => {
-            removeClientFromMap(user["first_name"], socket.id, roomId);
+        io.sockets
+            .in(roomId)
+            .emit(
+                "user-connected",
+                [...rooms[roomId]["students"].keys()],
+                [...rooms[roomId]["teachers"].keys()]
+            );
+
+        socket.on("class-end", (roomId) => {
+            removeClientFromMap(
+                user["first_name"],
+                socket.id,
+                roomId,
+                user["role"]
+            );
+
             io.sockets
                 .in(roomId)
-                .emit("user-disconnected", [...rooms[roomId].keys()]);
+                .emit(
+                    "leave-room",
+                    [...rooms[roomId]["students"].keys()],
+                    [...rooms[roomId]["teachers"].keys()]
+                );
+        });
+
+        socket.on("disconnect", () => {
+            removeClientFromMap(
+                user["first_name"],
+                socket.id,
+                roomId,
+                user["role"]
+            );
+            io.sockets
+                .in(roomId)
+                .emit(
+                    "user-disconnected",
+                    [...rooms[roomId]["students"].keys()],
+                    [...rooms[roomId]["teachers"].keys()]
+                );
         });
     });
 });
